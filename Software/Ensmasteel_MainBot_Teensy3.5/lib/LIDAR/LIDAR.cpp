8c7a0a4c51a6e64fe75c6978dbe802ff489e991d
Romain <tirelalangue98@laposte.net>
2019-10-31 00:13:31 +0100

    Lidar changed
    je sais pas si ça dérange que j'upload du code pas finis

--- Software/Ensmasteel_MainBot_Teensy3.5/lib/LIDAR/LIDAR.cpp
+++ Software/Ensmasteel_MainBot_Teensy3.5/lib/LIDAR/LIDAR.cpp
@@ -1,11 +1,35 @@
 #include "LIDAR.h"
 #include "Ghost.h"
 #include "Arduino.h"
+#include "RPLidar.h"
+
+
+#define delta_distance = 100;
+#define delta_angle = 10;
+
+#define dist_secondaryBot = 400;
+#define angle_secondaryBot = 90;
+
+#define dist_enemyBot = 1800;
+#define angle_enemyBot = 0;
+
 
 LIDAR::LIDAR(bool secondaryBot) // Constructor
 {
     SECONDARY_BOT_ALLY = secondaryBot;
 }
+
+
+void Begin(HardwareSerial &serialobj = Serial)
+{
+    return lidar.begin(&serialobj);
+
+    object_list[0] = enemyBot1;
+    object_list[0] = enemyBot2;
+    object_list[0] = allyBot2; 
+}
+
+Vector get_Vector(BotIdentificater id);
 
 Cinetique LIDAR::get_Cinetique(BotIdentificater id)
 {
@@ -19,4 +43,44 @@ bool LIDAR::trajectoryBlocked(bool forward)
     bool out;
 
     return out;
+}
+
+
+void detect()
+{
+    if (IS_OK(lidar.waitPoint())) { //take the data from rhe lidar
+        float distance = lidar.getCurrentPoint().distance;
+        float angle = lidar.getCurrentPoint().angle;
+    }
+
+    if (distance<max_dist){
+        //if object not too far reference the object
+
+        if (abs(distance-dist_secondaryBot)<delta_distance && abs(angle-angle_secondaryBot)<delta_angle)
+        {
+            //secondaryBot detected
+        }
+        else if(abs(distance-dist_secondaryBot)<delta_distance && abs(angle-angle_secondaryBot)<delta_angle)
+        {
+            //enemyBot detected, need to see if there are 2
+        }
+    }
+
+
+}
+
+
+
+void track()
+{
+    if (IS_OK(lidar.waitPoint())) { //take the data from rhe lidar
+        float distance = lidar.getCurrentPoint().distance;
+        float angle = lidar.getCurrentPoint().angle;
+    }
+
+    if (){
+        //if object not too far search corespondance with all previous objects
+    }
+
+
 }
--- Software/Ensmasteel_MainBot_Teensy3.5/lib/LIDAR/LIDAR.h
+++ Software/Ensmasteel_MainBot_Teensy3.5/lib/LIDAR/LIDAR.h
@@ -9,22 +9,72 @@
 
 #include "Arduino.h"
 #include "Vector.h"
+#include "RPLidar.h"
+
+
+#define min_radius 10 //the min radius of an objected necessary to be traced by lidar
+#define max_sep 5 //the maximum separation between 2 points of the same object
+#define max_dt //the maximum time between 2 detections of an object
+#define max_dist 2000 //the maximum distance concidered for detecting an object
+
+#define max_points_per_object 50
+#define max_objects_detected 20
+
+//first phase:
+//when robot are static --> detect them
+//second phase:
+//when robot are moving --> track them
+
+
+struct point
+{
+    float angle;
+    float distance;
+}
 
 enum BotIdentificater
 {
+    // How to diferenciate ?
+    // folow bot ==> blind spot
     enemyBot1,
     enemyBot2,
-    allyBot2
+    allyBot2,
+    tower,
+    unknown// if unknow object detected
+
 };
+
+//Lidar continously update object_data ==> will become a class soon
+struct object_data
+{
+    //data about the objects
+    BotIdentificater Bot_name = unknown;
+    Cinetique Bot_cinetique;
+
+    point old[max_objects_detected];//previous points associated to this object
+    int old_length = 0;
+    point now[max_objects_detected];//points being associated with this object
+    int now_length = 0;
+
+    float age; // age of the last detection of the object
+    bool state; //if true the object is tracked
+};
+
 
 class LIDAR
 {
 public:
+
     // GOAL / Constructor
     // IN   / bool secondaryBot : An ally is on the table => true
     LIDAR(bool secondaryBot = false);
 
+
+    //Serial conection with Lidar
+    bool Begin(HardwareSerial &serialobj = Serial);
+
     uint8_t botDetected = 0; //Number of enemy bot detected by the LIDAR
+
 
     // GOAL / Give speed and position of a specific bot identify by its identificater
     // IN   / BotIdentificater id
@@ -38,8 +88,20 @@ public:
     //               Free to move => false
     bool trajectoryBlocked(bool forward = true);
 
+    void detect(); //detect all non mooving objects
+
+    void track(); //identify all points too all previously known objects
+
 private:
+
     bool SECONDARY_BOT_ALLY = false;
+
+    RPLidar lidar;
+
+    //liste of all detected object
+    object_data object_list [max_objects_detected];
 };
+
+
 
 #endif
